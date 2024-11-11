e3f41d9f38c0c96accf343e5a7987952644f658a
RAVI-DAHIYA--00 <ravidahiya9350@gmail.com>
2024-11-11 21:32:17 +0530

    deep dive in classes

---
+++ oops in python/atm_machine.py
@@ -0,0 +1,13 @@
+# all the functions in ATM we will make will be methods
+
+class Atm :
+    
+    def __init__(self):
+        self.pin=""
+        self.balance=0  
+        
+        self.menu()
+    
+    def menu():
+        pass
+    
--- oops in python/classes.py
+++ oops in python/classes.py
@@ -1,9 +1,75 @@
+
 # we need class to define the different functionalities of our project in different classes.
+
+''' object is always of a class.
+
+  in python , data type is a class 
+            and the variable of that data type is a object  of that class .'''
+L=[1,2,3,4]                   #this L is a object of the (List) class.
+ 
+ 
+'''
+Class is a blueprint, blueprint of how an object will behave.
+
+        CLASS consist of two things :
+                1. Data or Property or attribute   :: description of the object.
+                2. Functions or behavior or method'''
 
 # syntax of class
 class example :                  #here class is to define class and example is the name of the class
     pass
 
+
+
+# basic structure of Class
+
+class Car :
+    color="blue"               #data
+    model="sports"            #data
+    
+    def calculateAvgSpeed(km,time):          #function or its behavior
+        pass
+        #some code 
+        
+        
+''' while making a Class name should be in Pascal Case
+ ThisIsPascalCase
+ 
+ function name and data name must be in snake case
+ this_is_snake_case
+ 
+ '''
+
+'''
+making a class
+   Class
+   Attributes
+   Methods
+   
+   in general method is data so it is private on;y to the class.    representation(-)
+   methods are public to the whole code.     representation(+)'''
+   
+   
+'''
+ Object is an Instance of the Class.
+ 
+ Examples of Object : 
+    Car(class)---------------->   swift(object)   //  swift=Car()
+    Sports(Class)-------------->   cricket(object)    // cricket=Sports()
+    
+    Dog=Animal()     this is how objects are created in python'''
+
+
+
+'''
+Functions vs Methods
+           1. methods are the special function thar are written inside the class.
+            
+           2. len(L)        this is a normal function anyone can use this function
+              L.append(1)    this append is the function that is defined under the List class.
+              
+            Function is generally available for everyone and anyone can call it.
+            Method is a function that is particularly defined in class and only the object of that class can access it. '''
 
 def my_skills1():
     print("we can access it directly by calling the function")
@@ -42,6 +108,13 @@ a = 10
 
 
 
+'''while declaring a variable inside the class we always do __init__ 
+and in input it gets self and then we declare our variables 
+    
+    class A:
+    def __init__(self):
+        self.pin=""
+        self.balance=0             '''
 
 
 class course:
@@ -54,6 +127,8 @@ class course:
         # here self is a pointer and we have made 3 variables here.
         ''' __init__ is a inbuilt variable it work is to provide data this is called as a constructor.
         it will provide data to class
+        
+        constructor is a special method in which the code automatically executes when the object of that class is made.
         '''
     def show_mentor(self):
         print(f"mentor name is {self.mentor}")
